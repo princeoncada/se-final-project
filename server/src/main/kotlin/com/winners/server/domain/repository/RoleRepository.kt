480c21a8aba49bb3bed112abb27945241338f813
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 02:56:56 +0800

    Repository: added findByName in RoleRepository

--- server/src/main/kotlin/com/winners/server/domain/repository/RoleRepository.kt
+++ server/src/main/kotlin/com/winners/server/domain/repository/RoleRepository.kt
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface RoleRepository: JpaRepository<Role, String> {
     fun findById(id: String, pageable: Pageable): Page<Role>
+    fun findByName(name: String): Optional<Role>
 }
