88b7d23a26fbadc89f9a682d51eb897f4087a85e
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 02:58:57 +0800

    Chore: remove unused imports

--- server/src/main/kotlin/com/winners/server/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/winners/server/config/SecurityConfig.kt
@@ -3,18 +3,15 @@ package com.winners.server.config
 import com.winners.server.config.service.JwtService
 import com.winners.server.config.service.StockOidcUserService
 import com.winners.server.config.service.StockUserDetailsService
-import com.winners.server.domain.repository.RefreshTokenRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 
