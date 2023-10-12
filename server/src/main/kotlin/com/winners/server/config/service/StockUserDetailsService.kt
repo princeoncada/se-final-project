598027e7b14787efdda499948c77c36a036a7d89
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 02:54:38 +0800

    Feature: custom userDetailsService now saves new users into DB

--- server/src/main/kotlin/com/winners/server/config/service/StockUserDetailsService.kt
+++ server/src/main/kotlin/com/winners/server/config/service/StockUserDetailsService.kt
@@ -1,17 +1,36 @@
 package com.winners.server.config.service
 
+import com.winners.server.domain.model.User
+import com.winners.server.domain.repository.RoleRepository
 import com.winners.server.domain.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import java.time.Instant
+import java.util.*
 
 @Service
 class StockUserDetailsService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val roleRepository: RoleRepository
 ): UserDetailsService {
     override fun loadUserByUsername(email: String): StockUserDetails {
-        val user = userRepository.findByEmail(email)
-        if (user.isEmpty) throw UsernameNotFoundException("User not found with email: $email")
-        return StockUserDetails(user.get())
+        try {
+            val user = userRepository.findByEmail(email)
+            if (user.isEmpty) throw UsernameNotFoundException("User not found with email: $email")
+            return StockUserDetails(user.get())
+        } catch (e: UsernameNotFoundException) {
+            val savedUser = userRepository.save(
+                User(
+                    id = UUID.randomUUID().toString(),
+                    role = roleRepository.findByName("USER").get(),
+                    email = email,
+                    password = "",
+                    createdAt = Instant.now(),
+                    updatedAt = Instant.now()
+                )
+            )
+            return StockUserDetails(savedUser)
+        }
     }
 }
