8a869c53ceab8851bd65b374ae19a01c7b24cd3e
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 02:48:50 +0800

    Refactor: included a state for page management

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -1,23 +1,27 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login.tsx";
-import React, {useState} from "react";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
-import RetrieveToken from "./pages/RetrieveToken.tsx";
+import RetrieveToken from "./redirect/RetrieveToken.tsx";
 import Cookies from "js-cookie";
+import Logout from "./redirect/Logout.tsx";
+import {useState} from "react";
 
 
 function Routing() {
+    const [authenticated, setAuthenticated] = useState(Boolean(Cookies.get("JWT")));
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={
-                    Cookies.get("JWT") == undefined ? <Login /> : <Home thisToken={Cookies.get("JWT")}/>
+                    authenticated ? <Home /> : <Login />
                 }/>
                 <Route path="/about" element={<About />}/>
                 <Route path="/contact" element={<Contact />}/>
-                <Route path="/retrieve-token" element={<RetrieveToken />}/>
+                <Route path="/retrieve-token" element={<RetrieveToken setAuthenticated={setAuthenticated}/>}/>
+                <Route path="/logout" element={<Logout setAuthenticated={setAuthenticated}/>}/>
             </Routes>
         </BrowserRouter>
     )
